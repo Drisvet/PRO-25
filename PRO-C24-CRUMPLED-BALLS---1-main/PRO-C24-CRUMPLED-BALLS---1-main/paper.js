6ff5d693e1cb3e8253170099f7c74b636a149a73
Drisvet <71675411+Drisvet@users.noreply.github.com>
2021-04-26 22:56:17 -0400

    Add files via upload

---
+++ PRO-C24-CRUMPLED-BALLS---1-main/PRO-C24-CRUMPLED-BALLS---1-main/paper.js
@@ -0,0 +1,12 @@
+class Paper{
+loadImage = paper("Picture/paper.png")
+radius = 70;
+
+if(paper.isTouching(dustbin)){
+    radius = 50;
+}
+
+class Dustbin{
+    loadImage = dustbin("Picture/dustbingreen.png")
+
+}
